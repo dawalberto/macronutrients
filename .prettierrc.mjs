30cd9aadf2fbe01db758784f344e491c76ac9a2c
Alberto García <albertoinformatica97@gmail.com>
2024-04-08 12:58:08 +0200

    react added and dashboard changed to react

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,19 +1,29 @@
-/** @type {import("prettier").Config} */
+const config = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	printWidth: 200,
+}
+
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-        trailingComma: "es5",
-        tabWidth: 4,
-        useTabs: true,
-        semi: false,
-        singleQuote: true,
-        jsxSingleQuote: true,
-        printWidth: 200,
-      },
-    },
-  ],
-};
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+				...config,
+			},
+		},
+		{
+			files: ['*.ts', '*.js', '*.tsx', '*.mjs'],
+			options: {
+				parser: 'typescript',
+				...config,
+			},
+		},
+	],
+}
