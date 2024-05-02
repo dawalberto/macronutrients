c4cb2f54ebfd6ddcb74c309962a9a9fa3d8ffb1b
Alberto García <albertoinformatica97@gmail.com>
2024-05-02 20:32:02 +0200

    astro config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
+	site: 'https://dawalberto.github.io',
+	base: 'macronutrients',
 })
